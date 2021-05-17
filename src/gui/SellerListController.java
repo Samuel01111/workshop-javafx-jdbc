67581dd2cf9c5d5e7e8f847108bd675b29460ee2
Samuel01111 <Samuel.nascimento.2015@hotmail.com>
2021-05-17 11:06:42 -0300

    Deparment ComboBox

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -31,6 +31,7 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
+import model.services.DepartmentService;
 import model.services.SellerService;
 
 public class SellerListController implements Initializable, DataChangeListener {
@@ -114,9 +115,9 @@ public class SellerListController implements Initializable, DataChangeListener {
 
 			SellerFormController controller = loader.getController();// controlador da tela
 			controller.setSeller(obj);
-			controller.setSeller(new SellerService());
-			controller.subscribeDataChangeListener(this); // me inscrevendo pra receber aquele evento de
-															// onDataChanged();
+			controller.setServices(new SellerService(), new DepartmentService());
+			controller.loadAssociatedObjects();
+			controller.subscribeDataChangeListener(this); // me inscrevendo pra receber aquele evento de												// onDataChanged();
 			controller.updateFormData();
 			// pra colocar um janela emcima da outra tem que instanciar um novo stage;
 			Stage dialogStage = new Stage();
@@ -127,6 +128,7 @@ public class SellerListController implements Initializable, DataChangeListener {
 			dialogStage.initModality(Modality.WINDOW_MODAL);
 			dialogStage.showAndWait();
 		} catch (IOException e) {
+			e.printStackTrace();
 			Alerts.showAlert("IO Exception", "Eroor loading view", e.getMessage(), AlertType.ERROR);
 		}
 	}
